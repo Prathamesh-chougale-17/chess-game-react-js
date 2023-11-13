03c45703e6fc0b2a5363a81f10ec8f1893f3cd09
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-11-13 21:18:32 +0530

    Fix flexbox styling and import statement

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,5 +1,5 @@
 import './Board.css'
-import { useAppContext }from '../../contexts/Context'
+import { useAppContext } from '../../contexts/Context'
 
 import Ranks from './bits/Ranks'
 import Files from './bits/Files'
@@ -12,36 +12,36 @@ import arbiter from '../../arbiter/arbiter'
 import { getKingPosition } from '../../arbiter/getMoves'
 
 const Board = () => {
-    const ranks = Array(8).fill().map((x,i) => 8-i)
-    const files = Array(8).fill().map((x,i) => i+1)
+    const ranks = Array(8).fill().map((x, i) => 8 - i)
+    const files = Array(8).fill().map((x, i) => i + 1)
 
     const { appState } = useAppContext();
     const position = appState.position[appState.position.length - 1]
 
     const checkTile = (() => {
-        const isInCheck =  (arbiter.isPlayerInCheck({
-            positionAfterMove : position,
-            player : appState.turn
+        const isInCheck = (arbiter.isPlayerInCheck({
+            positionAfterMove: position,
+            player: appState.turn
         }))
 
         if (isInCheck)
-            return getKingPosition (position, appState.turn)
+            return getKingPosition(position, appState.turn)
 
         return null
     })()
 
-    const getClassName = (i,j) => {
+    const getClassName = (i, j) => {
         let c = 'tile'
-        c+= (i+j)%2 === 0 ? ' tile--dark ' : ' tile--light '
-        if (appState.candidateMoves?.find(m => m[0] === i && m[1] === j)){
+        c += (i + j) % 2 === 0 ? ' tile--dark ' : ' tile--light '
+        if (appState.candidateMoves?.find(m => m[0] === i && m[1] === j)) {
             if (position[i][j])
-                c+= ' attacking'
-            else 
-                c+= ' highlight'
+                c += ' attacking'
+            else
+                c += ' highlight'
         }
 
         if (checkTile && checkTile[0] === i && checkTile[1] === j) {
-            c+= ' checked'
+            c += ' checked'
         }
 
         return c
@@ -49,31 +49,29 @@ const Board = () => {
 
     return <div className='board'>
 
-        <Ranks ranks={ranks}/>
+        <Ranks ranks={ranks} />
 
         <div className='tiles'>
-            {ranks.map((rank,i) => 
-                files.map((file,j) => 
-                    <div 
-                        key={file+''+rank} 
-                        i={i}
-                        j={j}
-                        className={`${getClassName(7-i,j)}`}>
+            {ranks.map((rank, i) =>
+                files.map((file, j) =>
+                    <div
+                        key={file + '' + rank}
+                        className={`${getClassName(7 - i, j)}`}>
                     </div>
                 ))}
         </div>
 
-        <Pieces/>
+        <Pieces />
 
         <Popup>
             <PromotionBox />
             <GameEnds />
         </Popup>
 
-        <Files files={files}/>
+        <Files files={files} />
 
     </div>
-    
+
 }
 
 export default Board
